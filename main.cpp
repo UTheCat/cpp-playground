2090b179ea9cac80b82a813947977620c0ca4bb6
UTheCat <bees9633@gmail.com>
2024-07-03 20:43:40 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,24 @@
 #include <iostream>
 using namespace std;
 
-int main(int, char**){
+int main(int, char**)
+{
     printf("Hello, from cpp-stuff!\n");
     cout << "This is another message." << endl;
+
+    bool should_exit = false;
+    while (should_exit == false)
+    {
+        cout << "What would you like to do? (If you would like to exit the program, type 'exit'.)" << endl;
+        
+        string action;
+        cin >> action;
+
+        if (action.compare("exit") == 0)
+        {
+            should_exit = true;
+        }
+    }
 
     return 0;
 }
