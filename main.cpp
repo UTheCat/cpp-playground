7ec9f4eeb659a402667463a0a2bfddb412c1fed4
UTheCat <bees9633@gmail.com>
2024-07-03 20:22:42 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main(int, char**){
     printf("Hello, from cpp-stuff!\n");
+    cout << "This is another message." << endl;
 
     return 0;
 }
