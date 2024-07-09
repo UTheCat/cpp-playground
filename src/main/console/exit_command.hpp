cb7f1d1ffa381759b975bcc4f87d8cb67f6cd98b
UTheCat <bees9633@gmail.com>
2024-07-08 19:57:43 -0700

    Create exit_command.hpp

---
+++ src/main/console/exit_command.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "console_command.hpp"
+
+#ifndef EXIT_COMMAND_H
+#define EXIT_COMMAND_H
+
+using namespace std;
+using namespace CppStuff;
+
+namespace CppStuff
+{
+    class ExitCommand : public ConsoleCommand
+    {
+
+    };
+}
+
+#endif // EXIT_COMMAND_H
