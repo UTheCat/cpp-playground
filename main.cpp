7b1f19ad5421aec6d6c9ee64159d0576c8999d26
UTheCat <bees9633@gmail.com>
2024-07-10 16:45:23 -0700

    use ExitCommand

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <iostream>
+#include "src/main/console/console_command.hpp"
+#include "src/main/console/exit_command.hpp"
 using namespace std;
-
+using namespace CppStuff;
 int main(int, char**)
 {
     printf("Hello, from cpp-stuff!\n");
     cout << "This is another message." << endl;
 
     bool should_exit = false;
+    ExitCommand exit_command = ExitCommand();
     while (should_exit == false)
     {
         printf("What would you like to do? (If you would like to exit the program, type 'exit'.)\n");
@@ -15,7 +18,7 @@ int main(int, char**)
         string action;
         cin >> action;
 
-        if (action.compare("exit") == 0)
+        if (exit_command.has_keyword(action))
         {
             should_exit = true;
         }
