c02fcc09f170c38ed9cd3410b790caf400b8c13e
UTheCat <bees9633@gmail.com>
2024-07-10 16:33:28 -0700

    Create console_command_group.cpp

---
+++ src/main/console/console_command_group.cpp
@@ -0,0 +1,19 @@
+#include "console_command_group.hpp"
+
+void ConsoleCommandGroup::run_command(string keyword)
+{
+    for (int i = 0; i < commands.size(); i++)
+    {
+        ConsoleCommand command = commands[i];
+        if (command.has_keyword(keyword))
+        {
+            command.run();
+            break;
+        }
+    }
+}
+
+ConsoleCommandGroup::ConsoleCommandGroup()
+{
+    commands = vector<ConsoleCommand>();
+}
