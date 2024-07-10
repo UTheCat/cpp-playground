2dbf11a085292a7caeffa7a84fc969e46ddb7259
UTheCat <bees9633@gmail.com>
2024-07-10 16:26:16 -0700

    Create console_command_group.hpp

---
+++ src/main/console/console_command_group.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include "console_command.hpp"
+
+using namespace std;
+using namespace CppStuff;
+
+#ifndef CONSOLE_COMMAND_GROUP_H
+#define CONSOLE_COMMAND_GROUP_H
+
+namespace CppStuff
+{
+    class ConsoleCommandGroup
+    {
+        public:
+        vector<ConsoleCommand> commands;
+
+        void run_command(string keyword);
+    };
+}
+
+#endif // CONSOLE_COMMAND_GROUP_H
