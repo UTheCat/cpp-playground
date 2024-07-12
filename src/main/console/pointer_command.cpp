33353884c223ff654a4a1557c2fd5e59613cbb35
UTheCat <bees9633@gmail.com>
2024-07-11 20:56:27 -0700

    fix PointerCommand

--- src/main/console/pointer_command.cpp
+++ src/main/console/pointer_command.cpp
@@ -4,17 +4,21 @@ void PointerCommand::run()
 {
     string pointer_name = "my_pointer";
 
+    int arbitrary_number = 31;
+
     int * my_pointer;
     //printf("Created a pointer named", pointer_name, ". Its memory address is", &my_pointer);
     printf(string("Created a pointer named ").append(pointer_name).append(string(". Its memory address is ")).c_str());
     cout << my_pointer;
+    cout << "\nThis pointer is currently uninitialized so the memory address stored in it is kinda random.";
+
+    my_pointer = &arbitrary_number;
+    printf("my_pointer is now storing the address of arbitrary_number. arbitrary_number is located at ");
+    cout << my_pointer;
     cout << "\n";
 
-    // Store something at the memory address held by my_pointer
+    // Store something at the memory address currently held by my_pointer
     *my_pointer = 38;
-
-    // Give memory back to the system (otherwise we'll end up with a memory leak)
-    delete my_pointer;
 }
 
 PointerCommand::PointerCommand()
