dfb718b6efbd67f79bd7597771363e57fbce8cb0
UTheCat <bees9633@gmail.com>
2024-07-25 21:15:06 -0700

    Create help_command.cpp

---
+++ src/main/console/help_command.cpp
@@ -0,0 +1,33 @@
+#include "help_command.hpp"
+
+HelpCommand::HelpCommand()
+{
+    keyword = "?";
+    aliases.push_back("help");
+}
+
+void HelpCommand::run()
+{
+    vector<ConsoleCommand*>* commands = &group.commands;
+    string output = "List of commands:\n";
+
+    for (int i = 0; i < commands->size(); i++)
+    {
+        string aliases_output = "";
+
+        ConsoleCommand* command = commands->at(i);
+        vector<string> aliases = command->aliases;
+
+        for (int i2 = 0; i2 < aliases.size(); i++)
+        {
+            aliases_output.append(aliases[i2]);
+
+            if (i2 < aliases.size() - 1)
+            {
+                aliases_output.append(", ");
+            }
+        }
+
+        output.append(keyword).append("\t").append(aliases_output).append("\n");
+    }
+}
