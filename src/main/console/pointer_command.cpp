20e0ccb6b6fdd4379fa6fba6bbfe268fd5e3efa6
UTheCat <bees9633@gmail.com>
2024-07-25 20:28:41 -0700

    Update pointer_command.cpp

--- src/main/console/pointer_command.cpp
+++ src/main/console/pointer_command.cpp
@@ -10,7 +10,7 @@ void PointerCommand::run()
     //printf("Created a pointer named", pointer_name, ". Its memory address is", &my_pointer);
     printf(string("Created a pointer named ").append(pointer_name).append(string(". Its memory address is ")).c_str());
     cout << my_pointer;
-    cout << "\nThis pointer is currently uninitialized so the memory address stored in it is kinda random.";
+    cout << "\nThis pointer is currently uninitialized so the memory address stored in it is kinda random.\n";
 
     my_pointer = &arbitrary_number;
     printf("my_pointer is now storing the address of arbitrary_number. arbitrary_number is located at ");
