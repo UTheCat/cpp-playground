e3028ca66e360f3f4f14a8cfdb100421a9bf49b8
UTheCat <bees9633@gmail.com>
2024-08-10 19:35:36 -0700

    Update linked_list_builder.hpp

--- src/main/linked_list/linked_list_builder.hpp
+++ src/main/linked_list/linked_list_builder.hpp
@@ -13,6 +13,8 @@ namespace CppStuff
         public:
         LinkedListNode first_item;
         
+        // Adding an ampersand (&) in front of the parameter name
+        // specifies that a *reference* to some value should be passed.
         LinkedListBuilder(vector<double*> &initial_items);
     };
 }
