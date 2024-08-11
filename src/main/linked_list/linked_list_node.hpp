f92dfb357628eb983ae2aa5e930b5edf27dad9ac
UTheCat <bees9633@gmail.com>
2024-08-10 19:12:26 -0700

    Create linked_list_node.hpp

---
+++ src/main/linked_list/linked_list_node.hpp
@@ -0,0 +1,29 @@
+
+#ifndef LINKED_LIST_NODE_H
+#define LINKED_LIST_NODE_H
+
+namespace CppStuff
+{
+    /**
+     * @brief A linked list node, containing some data and a pointer to the next item in the list
+     * 
+     */
+    class LinkedListNode
+    {
+        public:
+
+        /**
+         * @brief Value/data held by this node
+         * 
+         */
+        double val;
+
+        /**
+         * @brief Pointer to the next item
+         * 
+         */
+        LinkedListNode* next_item;
+    };
+}
+
+#endif // LINKED_LIST_NODE_H
