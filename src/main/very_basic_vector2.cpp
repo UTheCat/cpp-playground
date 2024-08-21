25ea7a154f379ca5a8eb96c22fc72427b82d51e9
UTheCat <bees9633@gmail.com>
2024-08-21 00:54:08 -0700

    Create very_basic_vector2.cpp

---
+++ src/main/very_basic_vector2.cpp
@@ -0,0 +1,11 @@
+#include "very_basic_vector2.hpp"
+
+using namespace CppStuff;
+
+VeryBasicVector2::VeryBasicVector2() : VeryBasicVector2((float) 0, (float) 0) { }
+
+VeryBasicVector2::VeryBasicVector2(float x, float y)
+{
+    this->x = x;
+    this->y = y;
+}
