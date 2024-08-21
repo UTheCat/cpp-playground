68b92cb9800fa36130adbd8f73738468e11b8410
UTheCat <bees9633@gmail.com>
2024-08-21 00:48:06 -0700

    Create very_basic_vector2.hpp

---
+++ src/main/very_basic_vector2.hpp
@@ -0,0 +1,21 @@
+#ifndef VERY_BASIC_VECTOR2_H
+#define VERY_BASIC_VECTOR2_H
+
+namespace CppStuff
+{
+    /**
+     * @brief A very basic Vector2, which stores two numbers named 'x' and 'y', and lets you do some operations with it.
+     * 
+     */
+    class VeryBasicVector2
+    {
+        public:
+        int x;
+        int y;
+
+        VeryBasicVector2();
+        VeryBasicVector2(int x, int y);
+    };
+}
+
+#endif // VERY_BASIC_VECTOR2_H
