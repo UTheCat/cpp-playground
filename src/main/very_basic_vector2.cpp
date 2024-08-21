a3b1f5b2ff21818fd3bb0f444ee958b92f4cb9d5
UTheCat <bees9633@gmail.com>
2024-08-21 00:54:29 -0700

    Update very_basic_vector2.cpp

--- src/main/very_basic_vector2.cpp
+++ src/main/very_basic_vector2.cpp
@@ -2,7 +2,7 @@
 
 using namespace CppStuff;
 
-VeryBasicVector2::VeryBasicVector2() : VeryBasicVector2((float) 0, (float) 0) { }
+VeryBasicVector2::VeryBasicVector2() : VeryBasicVector2((float)0, (float)0) {}
 
 VeryBasicVector2::VeryBasicVector2(float x, float y)
 {
