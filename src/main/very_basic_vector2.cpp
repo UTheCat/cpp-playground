3f09eda11f360040ea12730767ad74eece219d0e
UTheCat <bees9633@gmail.com>
2024-08-21 01:08:30 -0700

    implement operator overload

--- src/main/very_basic_vector2.cpp
+++ src/main/very_basic_vector2.cpp
@@ -9,3 +9,8 @@ VeryBasicVector2::VeryBasicVector2(float x, float y)
     this->x = x;
     this->y = y;
 }
+
+VeryBasicVector2 VeryBasicVector2::operator+(VeryBasicVector2* other)
+{
+    return VeryBasicVector2(x + other->x, y + other->y);
+}
