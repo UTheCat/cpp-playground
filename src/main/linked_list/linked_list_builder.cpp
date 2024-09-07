dbab3e0c6a342dee20072aa65996c57662383d80
UTheCat <bees9633@gmail.com>
2024-09-07 14:30:49 -0700

    change some things about LinkedListBuilder

--- src/main/linked_list/linked_list_builder.cpp
+++ src/main/linked_list/linked_list_builder.cpp
@@ -3,10 +3,18 @@
 
 using namespace CppStuff;
 
-// In the corresponding header file, "initial_items" is prefixed with an ampersand (&).
-// Adding an ampersand (&) in front of the parameter name
-// specifies that a *reference* to some value should be passed.
-LinkedListBuilder::LinkedListBuilder(vector<double> &initial_items)
+/**
+ * @brief Constructs a new LinkedListBuilder initialized with items from a vector.
+ * 
+ * In the corresponding header file, "initial_items" is prefixed with an ampersand (&).
+ * Adding an ampersand (&) in front of the parameter name
+ * specifies that a *reference* to some value should be passed.
+ * 
+ * @tparam T The type of the linked list's items
+ * @param initial_items The vector to get the items from
+ */
+template <typename T>
+LinkedListBuilder<T>::LinkedListBuilder(std::vector<T> &initial_items)
 {
     /*
     The previous "next item" pointer.
@@ -14,11 +22,11 @@ LinkedListBuilder::LinkedListBuilder(vector<double> &initial_items)
     This is set to nullptr initially so that the last item in the linked list
     has a terminating pointer.
     */
-    LinkedListNode* most_recent_next_item = nullptr;
+    LinkedListNode<T> * most_recent_next_item = nullptr;
 
-    for (int i = initial_items.size() - 1; i >= 0; i--)
+    for (std::size_t i = initial_items.size() - 1; i >= 0; --i)
     {
-        LinkedListNode node = LinkedListNode(initial_items[i], most_recent_next_item);
+        LinkedListNode<T> * node = new LinkedListNode<T>(initial_items[i], most_recent_next_item);
 
         if (i == 0)
         {
--- src/main/linked_list/linked_list_builder.hpp
+++ src/main/linked_list/linked_list_builder.hpp
@@ -1,21 +1,20 @@
 #include <vector>
 #include "linked_list_node.hpp"
 
-using namespace std;
-
 #ifndef LINKED_LIST_BUILDER_H
 #define LINKED_LIST_BUILDER_H
 
 namespace CppStuff
 {
+    template <typename T>
     class LinkedListBuilder
     {
         public:
-        LinkedListNode first_item;
+        LinkedListNode<T> * first_item;
         
         // Adding an ampersand (&) in front of the parameter name
         // specifies that a *reference* to some value should be passed.
-        LinkedListBuilder(vector<double> &initial_items);
+        LinkedListBuilder(std::vector<T> &initial_items);
     };
 }
 
