159da03fc8020505eddfa15e1ceba3d2cbc5af5b
UTheCat <bees9633@gmail.com>
2024-09-06 22:08:43 -0700

    Update very_basic_vector2.hpp

--- src/main/very_basic_vector2.hpp
+++ src/main/very_basic_vector2.hpp
@@ -5,30 +5,37 @@ namespace CppStuff
 {
     /**
      * @brief A very basic implementation of Vector2, which conventionally stores two numbers named 'x' and 'y', and lets you do some operations with it.
-     * 
+     *
      */
     class VeryBasicVector2
     {
-        public:
+    public:
         float x;
         float y;
 
         /**
          * @brief Default constructor.
-         * 
+         *
          * @param x The x-value of the vector (zero by default).
          * @param y The y-value of the vector (zero by default).
          */
         VeryBasicVector2(float x = 0.0F, float y = 0.0F);
 
         /**
-         * @brief Copy constructor.
-         * 
+         * @brief Copy constructor. Ampersand to the left of the parameter named 'other' indicates that an l-value reference should be passed.
+         *
          * @param other The VeryBasicVector2 to copy.
          */
-        VeryBasicVector2(const VeryBasicVector2 &other);
+        VeryBasicVector2(const VeryBasicVector2 & other);
 
-        VeryBasicVector2 operator+(VeryBasicVector2 &other);
+        /**
+         * @brief Move constructor. Double ampersand to the left of the parameter named 'other' indicates that an r-value should be passed.
+         *
+         * @param other
+         */
+        VeryBasicVector2(VeryBasicVector2 && other);
+
+        VeryBasicVector2 operator+(VeryBasicVector2 & other);
     };
 }
 
