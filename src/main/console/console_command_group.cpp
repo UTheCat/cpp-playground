63e2dee2841e4078e7a4fce5a8686d879e863fac
UTheCat <bees9633@gmail.com>
2024-09-06 23:22:21 -0700

    use ++i instead of i++

--- src/main/console/console_command_group.cpp
+++ src/main/console/console_command_group.cpp
@@ -2,7 +2,15 @@
 
 void ConsoleCommandGroup::run_command(string keyword)
 {
-    for (std::size_t i = 0; i < commands->size(); i++)
+    /*
+    size_t is recommended for iterating through a list in C++.
+    -------------------
+    ++i (pre-increment) is used instead of i++ (post-increment)
+    because post-increment creates a copy of the variable 'i', increments the copy by 1, and returns the copy.
+
+    This is unlike pre-increment, which only increments 'i' (the original variable in this context) by 1 without creating a copy of the variable.
+    */
+    for (std::size_t i = 0; i < commands->size(); ++i)
     {
         ConsoleCommand* command = commands->at(i);
         if (command->has_keyword(keyword))
