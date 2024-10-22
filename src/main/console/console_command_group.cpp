02d585a0095f8a622e933e5e64557d0f9d00e187
UTheCat <bees9633@gmail.com>
2024-10-22 14:16:29 -0700

    update console_command_group.cpp
    We can directly assign a vector to ConsoleCommandGroup.commands.
    
    Storing a pointer to that vector by allocating a vector to the heap is unnecessary because vectors already store their data on the heap.

--- src/main/console/console_command_group.cpp
+++ src/main/console/console_command_group.cpp
@@ -10,9 +10,9 @@ void ConsoleCommandGroup::run_command(string keyword)
 
     This is unlike pre-increment, which only increments 'i' (the original variable in this context) by 1 without creating a copy of the variable.
     */
-    for (std::size_t i = 0; i < commands->size(); ++i)
+    for (std::size_t i = 0; i < commands.size(); ++i)
     {
-        ConsoleCommand* command = commands->at(i);
+        ConsoleCommand* command = commands.at(i);
         if (command->has_keyword(keyword))
         {
             command->run();
@@ -23,14 +23,4 @@ void ConsoleCommandGroup::run_command(string keyword)
 
 ConsoleCommandGroup::ConsoleCommandGroup() = default;
 
-ConsoleCommandGroup::~ConsoleCommandGroup()
-{
-    // Delete the vector storing the ConsoleCommand instances when
-    // this object's destructor is called.
-    // 'delete' keyword is used because the vector is allocated on the heap,
-    // and the commands variable is storing a pointer to the vector.
-    delete commands;
-
-    // We don't want dangling pointers
-    commands = nullptr;
-}
+ConsoleCommandGroup::~ConsoleCommandGroup() = default;
