9896f4db73347778ba0f9916582d8462291bdf5b
UTheCat <bees9633@gmail.com>
2024-10-22 14:05:55 -0700

    modernize some code in pointer_command.cpp

--- src/main/console/pointer_command.cpp
+++ src/main/console/pointer_command.cpp
@@ -8,12 +8,13 @@ void PointerCommand::run()
 
     int *my_pointer;
     // printf("Created a pointer named", pointer_name, ". Its memory address is", &my_pointer);
-    printf(string("Created a pointer named ").append(pointer_name).append(string(". Its memory address is ")).c_str());
-    cout << my_pointer;
-    cout << "\nThis pointer is currently uninitialized so the memory address stored in it is kinda random.\n";
+    //printf(string("Created a pointer named ").append(pointer_name).append(string(". Its memory address is ")).c_str());
+    cout << "Created a pointer named" << pointer_name << ". Its memory address is "
+        << my_pointer
+        << "\nThis pointer is currently uninitialized so the memory address stored in it is kinda random.\n";
 
     my_pointer = &arbitrary_number;
-    printf("my_pointer is now storing the address of arbitrary_number. arbitrary_number is located at ");
+    cout << "my_pointer is now storing the address of arbitrary_number. arbitrary_number is located at ";
     cout << my_pointer;
     cout << "\n";
 
@@ -21,7 +22,7 @@ void PointerCommand::run()
     // This works because dereferencing a pointer returns an l-value reference to the object that the pointer is pointing to.
     *my_pointer = 38;
     cout << "The value stored at the address stored by my_pointer is now ";
-    cout << to_string(arbitrary_number);
+    cout << arbitrary_number;
     cout << "\n";
 
     // arbitrary_number in this context is stored on the stack, and will therefore be destroyed once
